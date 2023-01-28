b4f3047cf7595b75aa2a699fcdfd133279fa6692
alihaidar <alihaydar282828@gmail.com>
2023-01-28 13:42:30 +0200

    added object commented code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+"use strict";
 const container=document.querySelector('.container');
 const btns= document.querySelectorAll('.btn');
 const restartBtn=document.querySelector('.restartBtn');
@@ -21,7 +22,7 @@ let p = new Array(9).fill("z");
 
 function winner(c){
     if(p[0]===c && p[1]===c && p[2]===c || p[3]===c && p[4]===c & p[5]===c || p[6]===c && p[7]===c && p[9]===c || p[0]===c && p[3]===c && p[6]===c 
-        || p[1]===c && p[4]===c && p[7]===c || p[2]===c && p[5]===c && p[8]===c || p[0]===c && p[4]===c && p[8]===c || p[02]===c && p[4]===c && p[6]===c){
+        || p[1]===c && p[4]===c && p[7]===c || p[2]===c && p[5]===c && p[8]===c || p[0]===c && p[4]===c && p[8]===c || p[2]===c && p[4]===c && p[6]===c){
         console.log("true winner is "+ c);
         return true;
     }
@@ -122,3 +123,143 @@ restartBtn.addEventListener('click', function() {
     i=0;
 });
 
+
+/*
+"use strict";
+
+const Player=(name,sign)=>{
+    //this.sign=sign;
+    //this.name=name;
+
+    const getName =() => name;
+    /* another longer way
+    const getName=()=>{
+        return name;
+    }
+    
+    const getSign=()=>{
+        return sign;
+    }
+    return {getSign , getName};
+};
+
+const gameBoard = (()=>{ // hon gameBoard huwe object
+    const board=["","","","","","","","",""];
+
+    const setField=(index,sign)=>{
+        if(index > board.length) return;
+        board[index]=sign;
+    };
+
+    const getField=(index)=>{
+        if(index > board.length) return;
+        return board[index];
+    };
+
+    const reset = function(){
+        for(let i=0;i<board.length;i++){
+            board[i]="";
+        }
+    };
+
+    return { setField, getField , reset};
+
+})();
+
+const gameController = (()=>{
+
+    const playerX = Player("ali","X");
+    const playerO = Player("rawan","O");
+    let round=1;
+    let isOver=false;
+
+    const getIsOver = () =>{
+        return isOver;
+    };
+
+    const reset = ()=>{
+        round=1;
+        isOver = false;
+    }
+
+    const getCurrentPlayerSign = () =>{
+        return round % 2 === 1 ? playerX.getSign() : playerO.getSign(); 
+    };
+
+    const checkWinner = (fieldIndex)=>{
+        const winConditions=[
+            [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+        ];
+
+        return winConditions.filter((combination)=>combination.includes(fieldIndex))
+        .some((possibleCombination) => 
+            possibleCombination.every((index)=>gameBoard.getField(index)=== getCurrentPlayerSign())
+        );
+    };
+
+    const playRound = (fieldIndex)=>{
+        gameBoard.setField(fieldIndex, getCurrentPlayerSign());
+        if(checkWinner(fieldIndex)){
+            displayController.setResultMessage(getCurrentPlayerSign());
+            isOver = true;
+            return;
+        }
+        if(round ===9){
+            displayController.setResultMessage("draw");
+            isOver=true;
+            return;
+        }
+        round++;
+        displayController.setMessageElement(
+            `Player ${getCurrentPlayerSign()}'s turn`
+        );
+    };
+
+    return {playRound , getIsOver , reset};
+})();
+
+
+
+
+
+const displayController = (() =>{
+    const fieldElements= document.querySelectorAll('.btn');
+    const restartButton=document.querySelector('.restartBtn');
+    const messageElement=document.querySelector('.title');
+
+    fieldElements.forEach((field) =>
+        field.addEventListener("click",(e)=>{
+            if(gameController.getIsOver() || e.target.textContent !=="") return;
+            gameController.playRound(parseInt(e.target.dataset.index));
+            updateGameboard();
+        })
+    );
+
+    restartButton.addEventListener("click", (e) => {
+        gameBoard.reset();
+        gameController.reset();
+        updateGameboard();
+        setMessageElement("Player X's turn");
+      });
+    
+      const updateGameboard = () => {
+        for (let i = 0; i < fieldElements.length; i++) {
+          fieldElements[i].textContent = gameBoard.getField(i);
+        }
+      };
+    
+      const setResultMessage = (winner) => {
+        if (winner === "Draw") {
+          setMessageElement("It's a draw!");
+        } else {
+          setMessageElement(`Player ${winner} has won!`);
+        }
+      };
+    
+      const setMessageElement = (message) => {
+        messageElement.textContent = message;
+      };
+    
+      return { setResultMessage, setMessageElement };
+    })();
+*/
